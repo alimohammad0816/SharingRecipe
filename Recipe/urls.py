6388880f664d44c28d30d39d8ccf58047a03b4ac
maedejhn <maede.jahanian.7@gmail.com>
2023-07-29 12:14:21 +0430

    feat: create urls for category

--- Recipe/urls.py
+++ Recipe/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('chef/', include("chef.urls")),
     path('raw_material/', include("raw_material.urls")),
+    path('category/', include("category.urls")),
 ]
