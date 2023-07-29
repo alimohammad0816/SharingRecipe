89776bad4be6fe0b6613b84f31bfc8edbaad542f
maedejhn <maede.jahanian.7@gmail.com>
2023-07-29 11:14:48 +0430

    feat: create raw material admin

--- raw_material/admin.py
+++ raw_material/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from raw_material.serializers import *
 
-# Register your models here.
+
+@admin.register(RawMaterial)
+class RawMaterialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'amount_of']
