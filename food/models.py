0bb59e85387e7613e1ce282e609d82afbade9760
maedejhn <maede.jahanian.7@gmail.com>
2023-07-30 20:10:41 +0430

    feat:create serializer for food

--- food/models.py
+++ food/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from raw_material.models import RawMaterial
+from category.models import Category
 
-# Create your models here.
+
+class Food(models.Model):
+    class RankChoices(models.TextChoices):
+        very_bad_taste = 1, 'very_bad'
+        bad_taste = 2, 'bad'
+        usual_taste = 3, 'usual'
+        yummy_taste = 4, 'yummy'
+        delicious_taste = 5, 'delicious'
+
+    raw_materials = models.ManyToManyField(RawMaterial)
+    foods_category = models.ManyToManyField(Category)
+    name = models.CharField(max_length=200)
+    recipe = models.TextField()
+    ranking = models.CharField(choices=RankChoices.choices, null=True, blank=True, max_length=10)
+
+
