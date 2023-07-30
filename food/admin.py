aed74446b45f25080f13d775ea7cf7c1f735456a
maedejhn <maede.jahanian.7@gmail.com>
2023-07-30 20:36:33 +0430

    feat:create admin for food

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from food.models import *
 
-# Register your models here.
+
+class FoodMaterialAdmin(admin.TabularInline):
+    model = FoodMaterialMemberShip
+    extra = 1
+
+
+class FoodCategoryAdmin(admin.TabularInline):
+    model = FoodCategoryMemberShip
+    extra = 1
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'recipe', 'ranking', 'raw_materials', 'food_category']
+    list_filter = ['ranking', 'food_category']
+
+    inlines = (FoodMaterialAdmin, FoodCategoryAdmin,)
