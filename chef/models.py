70a9e962ab9a75050199bcc837363a3405f3d62d
maedejhn <maede.jahanian.7@gmail.com>
2023-07-31 19:54:51 +0430

    fix: fix validator for pass & email

--- chef/models.py
+++ chef/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 
 class Chef(AbstractUser):
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=150)
     username = models.CharField(max_length=70, unique=True)
-    email_regex = r"^\S+@\S+\.\S+$"
-    email = models.EmailField(validators=email_regex)
-    password_regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-    password = models.CharField(max_length=20, validators=password_regex)
+    email = models.EmailField(validators=[
+        RegexValidator(
+            regex=r"^\S+@\S+\.\S+$",
+            message='email is not valid',
+        ),
+    ])
+    password = models.CharField(max_length=20, validators=[
+        RegexValidator(
+            regex="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+            message='password is not good',
+        ),
+    ])
