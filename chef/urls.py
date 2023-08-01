15807850bd556cd51c38dde9422768079670bc7e
maedejhn <maede.jahanian.7@gmail.com>
2023-08-01 11:10:45 +0430

    feat:add login

--- chef/urls.py
+++ chef/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
-from chef.views import RegisterView, MyObtainTokenPairView
-from rest_framework_simplejwt.views import TokenRefreshView
-
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('list/', views.ChefListAPIView.as_view()),
+    path('createlist/', views.ChefCreateListAPIView.as_view()),
     path('details/<int:pk>/', views.ChefDetailsAPIView.as_view()),
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('login/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
--- chef/views.py
+++ chef/views.py
@@ -1,14 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from chef.serializers import *
-from chef.serializers import MyTokenObtainPairSerializer
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
-from chef.serializers import RegisterSerializer
-from rest_framework import generics
 from chef.models import Chef
 
 
-class ChefListAPIView(ListAPIView):
+class ChefCreateListAPIView(ListCreateAPIView):
     queryset = Chef.objects.all()
     serializer_class = ChefSerializer
 
@@ -18,12 +13,4 @@ class ChefDetailsAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ChefSerializer
 
 
-class MyObtainTokenPairView(TokenObtainPairView):
-    permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
 
-
-class RegisterView(generics.CreateAPIView):
-    queryset = Chef.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
